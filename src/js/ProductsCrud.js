42412c96b27c46073ed43ddde5b1750d3263903a
Konstantin Beloglazov <ajoq@ya.ru>
2022-09-27 14:51:31 +0300

    js 2

--- src/js/ProductsCrud.js
+++ src/js/ProductsCrud.js
@@ -20,20 +20,61 @@ export default class ProductsCrud {
     }
 
     init() {
-        this.eventButtons = document.querySelectorAll('.event-button')
-        this.form = document.getElementById('modal-form');
+        this.form = document.forms.addUpdateProduct;
         this.modal = document.querySelector('.modal');
         this.modalSaveBtn = document.querySelector('.modal-form__buttons_save');
+        this.modalCanselBtn = document.querySelector('.modal-form__buttons_reset');
         this.products = document.querySelector('.products');
         this.producstList = document.querySelector('.products-list__items');
         this.productsAddButton = document.querySelector('.products-header__add');
+        
+        this.editProductId = null;
+        this.editProductArrIndex = null;
+        this.lastProductId = 3;
 
         this.events();
         this.updateList();
     }
+    
+    cancel(e) {
+        e.preventDefault();
+
+        this.form.reset();
+        this.modal.classList.remove('show');
+
+        this.eventsEnable();
+        this.editProductId = null;
+        this.editProductArrIndex = null;
+    }
 
     events() {
-        this.modalSaveBtn.addEventListener('click', (e) => this.saveProduct(e));
+        this.modalSaveBtn.addEventListener('click', (e) => this.productSave(e));
+        this.modalCanselBtn.addEventListener('click', (e) => this.cancel(e));
+        this.producstList.addEventListener('click', (e) => this.eventsEditDelete(e));
+        this.eventsOnce();
+    }
+    
+    eventsDisable() {
+        this.eventButtons = document.querySelectorAll('.event-button');
+        Array.from(this.eventButtons).forEach(item => {
+            item.classList.add('btn-disable');
+        });
+    }
+
+    eventsEditDelete(e) {
+        if (e.target.classList.contains('edit-btn')) {
+            this.productEdit(e.target);
+        };
+        if (e.target.classList.contains('delete-btn')) {
+            this.productDelete(e.target);
+        };
+    }
+    
+    eventsEnable() {
+        this.eventBtns = document.querySelectorAll('.event-button');
+        Array.from(this.eventBtns).forEach(item => {
+            item.classList.remove('btn-disable');
+        })
         this.eventsOnce();
     }
     
@@ -41,37 +82,47 @@ export default class ProductsCrud {
         this.productsAddButton.addEventListener('click', () => this.showModal(), {once: true});
     }
 
-    eventsDisable() {
-        Array.from(this.eventButtons).forEach(item => {
-            item.classList.add('btn-disable');
-        });
+    productEdit(tag) {
+        this.editProductId = +(tag.closest('div.products-list__item').dataset.id);
+        this.editProductArrIndex = this.productsArr.findIndex((item) => item.id === this.editProductId);
+
+        this.showModal();
+        this.form.elements.name.value = this.productsArr[this.editProductArrIndex].name;
+        this.form.elements.price.value = this.productsArr[this.editProductArrIndex].price;
     }
 
-    eventsEnable() {
-        Array.from(this.eventButtons).forEach(item => {
-            item.classList.remove('btn-disable');
-        });
-        this.eventsOnce();
+    productDelete(tag) {
+        const delProductId = +(tag.closest('div.products-list__item').dataset.id);
+        const delProductArrIndex = this.productsArr.findIndex((item) => item.id === delProductId);
+        this.productsArr.splice(delProductArrIndex, 1);
+        this.updateList();
     }
 
-    saveProduct(e) {
+    productSave(e) {
         e.preventDefault();
-        // console.log(this.productsArr.length);
         let formData = new FormData(this.form);
-        // for(let [name, value] of formData) {
-        //     console.log(`${name} = ${value}`);
- 
-        //     } 
         const product = {
-            id: +(this.productsArr.length + 1),
+            id: +(this.lastProductId + 1),
             name: formData.get('product_name'),
             price: +(formData.get('product_price')),
         }
-        // console.log(product);
-        this.productsArr.push(product);
-        // console.log(this.productsArr);
-        this.updateList();
 
+        if (this.editProductId) {
+            product.id = this.editProductId;
+            this.productsArr.splice(this.editProductArrIndex, 1, product);
+            this.editProductId = null;
+            this.editProductArrIndex = null;
+
+        } else {
+            this.productsArr.push(product);
+            this.lastProductId += 1;
+        }
+
+        this.form.reset();
+        this.modal.classList.remove('show');
+
+        this.eventsEnable();
+        this.updateList();
     }
 
     showModal() {
@@ -93,8 +144,8 @@ export default class ProductsCrud {
                 <div class="products-name">${item.name}</div>
                 <div class="products-price">${item.price}</div>
                 <div class="products-actions">
-                    <span class="products-actions__item event-button">✎</span>
-                    <span class="products-actions__item event-button">✕</span>
+                    <span class="products-actions__item edit-btn event-button">✎</span>
+                    <span class="products-actions__item delete-btn event-button">✕</span>
                 </div>            
             `;
             
